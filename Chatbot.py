e2c932e5420468c6db554c0db1ba8d77697bc731
RB <rajbirru@users.noreply.github.com>
2024-05-04 08:35:20 -0400

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -6,7 +6,7 @@ with st.sidebar:
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
-openai_api_key = st.secrets["OPEN_API_KEY"]
+openai_api_key = st.secrets["OPENAI_API_KEY"]
 st.title("💬 Chatbot")
 st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
 if "messages" not in st.session_state:
